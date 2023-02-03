2d1a65585e6d300d5d79efd42887311577b7cda9
김민성 <sohoung4@gmail.com>
2023-02-03 17:07:17 +0900

    투 포인터

---
+++ Do-it/src/배열과_리스트/배열/투포인터.java
@@ -0,0 +1,22 @@
+package 배열과_리스트.배열;
+import java.util.Scanner;
+
+// 설명 : 어떠한 자연수 n은 몇 개의 연속된 자연수의 합으로 나타낼 수 있다. 당신은 어떤 자연수 n을 몇 개의 연속된 자연수의 합으로 나타내는 가짓수를 알고 싶다.
+//       이 때 사용하는 자연수는 n이여야하고 n을 입력받아 연속된 자연수의 합으로 나타내는 가짓수를 출력하는 프로그램을 작성하세요.
+// 입력 예시 : 15
+// 출력 예시 : 4
+
+public class 투포인터 {
+    public static void main(String[] args) {
+        투포인터 t = new 투포인터();
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int[] arr = new int[n+1];
+        int answer = 0;
+        int sum = 0;
+        int lt = 0;
+        
+
+        System.out.println(answer);
+    }
+}
