a1d321a2b9c794bd4355d3c262314056a71e0eb7
김민성 <sohoung4@gmail.com>
2023-02-06 15:43:53 +0900

    no message

--- Do-it/src/배열과_리스트/배열/투포인터.java
+++ Do-it/src/배열과_리스트/배열/투포인터.java
@@ -1,4 +1,5 @@
 package 배열과_리스트.배열;
+
 import java.util.Scanner;
 
 // 설명 : 어떠한 자연수 n은 몇 개의 연속된 자연수의 합으로 나타낼 수 있다. 당신은 어떤 자연수 n을 몇 개의 연속된 자연수의 합으로 나타내는 가짓수를 알고 싶다.
@@ -10,13 +11,24 @@ public class 투포인터 {
     public static void main(String[] args) {
         투포인터 t = new 투포인터();
         Scanner sc = new Scanner(System.in);
-        int n =sc.nextInt();
-        int[] arr = new int[n+1];
-        int answer = 0;
-        int sum = 0;
-        int lt = 0;
-        
-
+        int n = sc.nextInt();
+        int answer = 0;  // 출력을 위한
+        int sum = 1;     // 입력된 n과 비교해야 하는 변수
+        int lt = 1;      // 시작 인덱스
+        int rt = 1;      // 끝 인덱스
+        while (rt != n) {   // while문의 조건으로 n이 끝 인덱스가 되기 전까지
+            if (sum == n) { // sum이 입력된 n과 같다면
+                rt++;       // 다음 비교를 위해서 rt를 1증가해주고
+                sum += rt;  // sum에 누적합해주고 answer을 1증가시킨다.
+                answer++;
+            } else if (sum > n) {  // sum이 입력된 n보다 크다면
+                sum -= lt;         // lt값을 먼저 빼주고 다음 비교를 위해서 lt를 1증가
+                lt++;
+            } else {               // sum이 입력된 n보다 작다면
+                rt++;              // rt를 1증가시켜주고 sum에 rt값을 1 증가
+                sum += rt;
+            }
+        }
         System.out.println(answer);
     }
 }
