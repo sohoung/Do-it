a255bd122f67f1554500887439f9b89a2ecc3af3
김민성 <sohoung4@gmail.com>
2023-02-07 16:17:18 +0900

    no message

---
+++ Do-it/src/배열과_리스트/배열/DNA.java
@@ -0,0 +1,10 @@
+package 배열과_리스트.배열;
+import java.util.Scanner;
+
+// 설명 : 
+public class DNA {
+    public static void main(String[] args) {
+        DNA t = new DNA();
+        Scanner sc = new Scanner(System.in);
+    }
+}
