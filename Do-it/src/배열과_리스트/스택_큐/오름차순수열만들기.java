6e1fcae7024a387a202056a5b0a205d05535c988
김민성 <sohoung4@gmail.com>
2023-02-11 23:13:44 +0900

    no message

--- Do-it/src/배열과_리스트/스택_큐/오름차순수열만들기.java
+++ Do-it/src/배열과_리스트/스택_큐/오름차순수열만들기.java
@@ -1,8 +1,16 @@
 package 배열과_리스트.스택_큐;
+import java.util.Scanner;
 
 public class 오름차순수열만들기 {
     public static void main(String[] args) {
         오름차순수열만들기 t = new 오름차순수열만들기();
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            System.out.println();
+        }
+
     }
 }
