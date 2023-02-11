741ecb7c913fc51e2bdeb940be6739e589d97228
김민성 <sohoung4@gmail.com>
2023-02-11 20:49:26 +0900

    no message

---
+++ Do-it/src/배열과_리스트/스택_큐/오름차순수열만들기.java
@@ -0,0 +1,8 @@
+package 배열과_리스트.스택_큐;
+
+public class 오름차순수열만들기 {
+    public static void main(String[] args) {
+        오름차순수열만들기 t = new 오름차순수열만들기();
+        Scanner sc = new Scanner(System.in);
+    }
+}
